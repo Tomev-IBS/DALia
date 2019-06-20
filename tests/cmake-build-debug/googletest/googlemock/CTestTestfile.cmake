437117c913e5a9077d9cc19ba2e3d90966e58b94
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-06-20 16:43:27 +0200

    Update of googletest and trash removal.

--- tests/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Coding/C++/DALia/tests/googletest/googlemock
-# Build directory: D:/Coding/C++/DALia/tests/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- tests/cmake-build-debug/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Coding/C++/DALia/tests/googletest/googletest
-# Build directory: D:/Coding/C++/DALia/tests/cmake-build-debug/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
