25ac197db96ea33c67d575b93181c735a507d59c
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-08 21:31:46 +0200

    Added required methods to i_inferer.h.

--- src/inference/i_inferer.h
+++ src/inference/i_inferer.h
@@ -17,10 +17,26 @@ namespace DALia {
 
   class i_inferer {
     public:
-      virtual void infer(std::vector<rulePtr> rules) = 0;
-      virtual std::unordered_map<std::string, std::string> getReport() = 0;
-    private:
-      virtual ~i_inferer();
+      // Note that it has to be passed as a copy, as most inferers will remove rules that were fired during inference.
+      virtual void infer(std::vector<rulePtr> rules, std::unordered_map<std::string, std::vector<std::string>> facts) = 0;
+      // A few information has to be provided for experiments
+      virtual std::string getInferenceType() = 0;
+      virtual unsigned int getNumberOfRulesThatCouldInitiallyBeFired() = 0;
+      virtual bool wasInferenceTargetSet() = 0;
+      virtual bool wasInferenceTargetInitiallyConfirmable() = 0;
+      virtual unsigned int getNumberOfInferenceIterations() = 0;
+      virtual bool wasAnyRuleFired() = 0;
+      virtual unsigned int getNumberOfRulesFired() = 0;
+      virtual unsigned int getNumberOfNewFacts() = 0;
+      // Note that inference target is also confirmed in case where no target was assigned, but a rule was fired.
+      virtual bool wasInferenceTargetConfirmed() = 0;
+      virtual bool wasNewKnowledgeExplored() = 0;
+      // Note that inference can be done on different kind of objects (e.g. clusters)
+      virtual int getNumberOfCheckedObjects() = 0;
+      virtual int getNumberOfRulesThatCouldBeFired() = 0;
+      virtual double getInferenceTime() = 0;
+
+      virtual ~i_inferer() = delete;
   };
 
   typedef std::shared_ptr<i_inferer> infererPtr;
