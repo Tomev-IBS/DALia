0481dd4a3ec5f26304773af0a6678f679e0779ee
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-07-09 19:09:04 +0200

    Removed test.cpp and its test.

--- tests/run_all_tests.cpp
+++ tests/run_all_tests.cpp
@@ -1,11 +1,6 @@
-#include "test.cpp"
 #include "gtest/gtest.h"
 
-TEST(BasicTests, AdditionTest){
-  EXPECT_EQ(4, add(2, 2));
-}
-
-int main(void){
+int main(){
   testing::InitGoogleTest();
   return RUN_ALL_TESTS();
 }
